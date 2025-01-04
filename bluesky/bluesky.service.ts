c2d2750e1460ba0c7e2dbe6ce6d41bfed9aef89d
Preston Howell <pshowell23@gmail.com>
2025-01-03 20:10:51 -0600

    Add bluesky service

---
+++ bluesky/bluesky.service.ts
@@ -0,0 +1,33 @@
+import "jsr:@std/dotenv/load";
+
+import { AtpAgent } from "npm:@atproto/api";
+
+const agent = new AtpAgent({
+    service: 'https://bsky.social'
+});
+
+export class SpudzBotBlueskyService {
+    async login() {
+        const user: string = Deno.env.get("BLUESKY_USERNAME") || '';
+        const pass: string = Deno.env.get("BLUESKY_PASSWORD") || '';
+
+        if (!user || !pass) {
+            throw new Error("BLUESKY_USERNAME or BLUESKY_PASSWORD is not set in the environment.");
+        }
+
+        await agent.login({ identifier: user, password: pass });
+
+        console.log('Logged in to Bsky.social');
+    }
+    
+    async goodMorning() {
+        try {
+
+            await agent.post({
+                text: 'Good morning friends!'
+            });
+        } catch {
+            console.error('Failed to post to Bsky.social');
+        }
+    }
+}
